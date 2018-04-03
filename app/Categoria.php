fe85b6d2edb4653b9bcf9b82f2ba1d24e82693fa
Sebastian Artaza <sartaza@summasolutions.net>
2018-04-03 00:23:53 -0300

    fix categorias

--- app/Categoria.php
+++ app/Categoria.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    protected $table = 'Categorias';
+    protected $table = 'categorias';
     public $timestamps = true;
     protected $fillable = [
             'name',
